f7fc9041aea00f499c4118749ff4c6ee13648f38
Sandhu <anusandhu666@gmail.com>
2022-12-25 19:28:49 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,10 @@
+document.querySelector('.dropdown').addEventListener('mouseover',()=>{
+        document.querySelector('.drop').style.display='block';
+
+    
+})
+
+document.querySelector('.dropdown').addEventListener('mouseout',()=>{
+        document.querySelector('.drop').style.display='none';
+    
+})
